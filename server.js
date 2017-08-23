69609a39c43af41f70b49cb52c78b8c532785152
pnegi <prinegi100@gmail.com>
2017-08-23 17:07:11 +0530

    created GET /bookmarks, helper lib, validations of arguments

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const config = require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
-const v1Routes = require('./routes/v1'); 
+
 const port = process.env.PORT;
+const v1Routes = require('./routes/v1'); 
+
 
 const app = express();
 
+// middlewares
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+// route middleware
 app.use('/api/v1', v1Routes);
 
 app.listen(port, () => {
