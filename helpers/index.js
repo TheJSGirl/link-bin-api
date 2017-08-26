6360ce2b1d0d4df24205464e34fb8965728429c6
pnegi <prinegi100@gmail.com>
2017-08-26 09:55:12 +0530

    replaced response with sendResponse()

--- helpers/index.js
+++ helpers/index.js
@@ -2,7 +2,11 @@ module.exports = {
   verifyLink: (link) => {
     return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(link);
   },
-
+  /**
+   *  Function : sendResponse()
+   * @param {object} res : the response object of route
+   * @param {array} data : the data array from mysql by Sequelize
+   */
   sendResponse: (res, data = [], status, message, code) => {
     let errCode = code;
 
