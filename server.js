054b9ff3e59b941706660fc8728dc1ae6567e286
pnegi <prinegi100@gmail.com>
2017-09-07 18:01:17 +0530

    added index file

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors  =require('cors');
+const path = require('path');
 
 const port = process.env.PORT;
 const v1Routes = require('./routes/v1'); 
+
+const staticFilePath = path.join(__dirname, './public' );
 
 
 const app = express();
@@ -16,8 +19,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
+//serving static file
+app.use(express.static(staticFilePath));
+
 // route middleware
 app.use('/api/v1', v1Routes);
+
+app.get('/', (req, res) => {
+  return res.sendFile(staticFilePath)
+})
 
 app.listen(port, () => {
   console.log('server is running on port no. :'+ port);
